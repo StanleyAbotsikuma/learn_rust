28ad52971e32233e7242fa23d77822617ec66c1d
StanleyAbotsikuma <abotsistanley0@gmail.com>
2022-10-09 23:21:38 +0000

    returning value

--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,10 @@
-use rand::Rng;
-use std::io;
 fn main() {
-    while "start"== "start" {
-        println!("");
-        println!("");
-        println!("Enter a number from 1 - 10");
-        println!("###########################");
-        let mut input = String::new();
+    let mut testString = String::new();
+    testString = "stanley".to_string();
+println!("the returning value is {}",test(testString))
+}
 
-        io::stdin()
-            .read_line(&mut input)
-            .expect("failed to read line");
-        
-        let num = rand::thread_rng().gen_range(0..10);
-        if input.trim().parse::<u32>().unwrap() == num {
-            println!("Great well done the predicted number was {}", num);
-        } else {
-            println!("Sorry try again the predicted number was {}", num);
-        }
-    }
+fn test(params:String )-> String
+{
+return params;
 }
