de7d0240bc48335c5a2fe14a0cda8a3deb17ca43
StanleyAbotsikuma <abotsistanley0@gmail.com>
2022-10-25 21:32:51 +0000

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,32 @@
-fn main() {
-  
+use postgres::{Client, NoTls};
+struct Person {
+    id: i32,
+    name: String,
+    data: Option<Vec<u8>>
 }
+fn main() {
+//     let conn = Connection::connect("postgresql://postgres@localhost:5433", TlsMode::None)
+//     .unwrap();
 
+// conn.execute("CREATE TABLE person (
+//             id              SERIAL PRIMARY KEY,
+//             name            VARCHAR NOT NULL,
+//             data            BYTEA
+//           )", &[]).unwrap();
+// let me = Person {
+// id: 0,
+// name: "Steven".to_owned(),
+// data: None
+// };
+// conn.execute("INSERT INTO person (name, data) VALUES ($1, $2)",
+//          &[&me.name, &me.data]).unwrap();
+
+// for row in &conn.query("SELECT id, name, data FROM person", &[]).unwrap() {
+// let person = Person {
+//     id: row.get(0),
+//     name: row.get(1),
+//     data: row.get(2)
+// };
+// println!("Found person {}", person.name);
+// }
+}
