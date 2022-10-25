1109b3465cf7c625545f9ccbe464521bb8c04328
StanleyAbotsikuma <abotsistanley0@gmail.com>
2022-10-25 00:32:40 +0000

    db

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,4 @@
 fn main() {
-    let mut testString = String::new();
-    testString = "stanley".to_string();
-println!("the returning value is {}",test(testString));
-let x = 5;
-let y = 5003321;
-println!("{} + {} is {}",x , y ,add(x,y));
+  
 }
 
-fn test(params:String )-> String
-{
-return params;
-}
-fn add( x:u32, y:u32)->u32
-{
-    return x + y ;
-}
