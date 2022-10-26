69370523063fd6fc27deea3753f204dfd038be29
StanleyAbotsikuma <abotsistanley0@gmail.com>
2022-10-26 05:10:47 +0000

    rust

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,30 @@
-use postgres::{Client, NoTls};
-struct Person {
-    id: i32,
-    name: String,
-    data: Option<Vec<u8>>
+use postgres::{Client, Error, NoTls};
+fn main() -> Result<(), Error> {
+    let mut client = Client::connect(
+        "postgres://postgres:root@localhost:5432/postgres",
+        NoTls,
+    )?;
+    client.batch_execute("
+    CREATE TABLE personname (
+        id      SERIAL PRIMARY KEY,
+        name    TEXT NOT NULL,
+        data    BYTEA
+    )
+")?;
+
+let name = "Ferris";
+let data = None::<&[u8]>;
+client.execute(
+    "INSERT INTO person (name, data) VALUES ($1, $2)",
+    &[&name, &data],
+)?;
+
+for row in client.query("SELECT id, name, data FROM person", &[])? {
+    let id: i32 = row.get(0);
+    let name: &str = row.get(1);
+    let data: Option<&[u8]> = row.get(2);
+
+    println!("found person: {} {} {:?}", id, name, data);
 }
-fn main() {
-//     let conn = Connection::connect("postgresql://postgres@localhost:5433", TlsMode::None)
-//     .unwrap();
-
-// conn.execute("CREATE TABLE person (
-//             id              SERIAL PRIMARY KEY,
-//             name            VARCHAR NOT NULL,
-//             data            BYTEA
-//           )", &[]).unwrap();
-// let me = Person {
-// id: 0,
-// name: "Steven".to_owned(),
-// data: None
-// };
-// conn.execute("INSERT INTO person (name, data) VALUES ($1, $2)",
-//          &[&me.name, &me.data]).unwrap();
-
-// for row in &conn.query("SELECT id, name, data FROM person", &[]).unwrap() {
-// let person = Person {
-//     id: row.get(0),
-//     name: row.get(1),
-//     data: row.get(2)
-// };
-// println!("Found person {}", person.name);
-// }
+    Ok(())
 }
