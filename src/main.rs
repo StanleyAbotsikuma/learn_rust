cbd65c7209e25f0ecc22780fe28653b5d78d671d
StanleyAbotsikuma <abotsistanley0@gmail.com>
2022-10-26 10:00:38 +0000

    cli crud title

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,28 @@
 use postgres::{Client, Error, NoTls};
+use std::io;
+
+
+
 fn main() -> Result<(), Error> {
-    let mut client = Client::connect(
+    let mut client:Client = Client::connect(
         "postgres://postgres:root@localhost:5432/postgres",
         NoTls,
     )?;
-    client.batch_execute("
-    CREATE TABLE personname (
-        id      SERIAL PRIMARY KEY,
-        name    TEXT NOT NULL,
-        data    BYTEA
-    )
-")?;
+
+    println!("++++++++++++++++++++++++++++++++++");
+    println!("Welcome to Rust Database CLI CRUD");
+    println!("++++++++++++++++++++++++++++++++++");
+    println!("");
+    while true{
+        
+    }
+//     client.batch_execute("
+//     CREATE TABLE personname (
+//         id      SERIAL PRIMARY KEY,
+//         name    TEXT NOT NULL,
+//         data    BYTEA
+//     )
+// ")?;
 
 let name = "Ferris";
 let data = None::<&[u8]>;
@@ -18,6 +30,8 @@ client.execute(
     "INSERT INTO person (name, data) VALUES ($1, $2)",
     &[&name, &data],
 )?;
+
+execution(&client,name,data);
 
 for row in client.query("SELECT id, name, data FROM person", &[])? {
     let id: i32 = row.get(0);
