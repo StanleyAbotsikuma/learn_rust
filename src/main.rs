e3ed54b393ee0a87a8f732cd8b0f4de7270d66a8
StanleyAbotsikuma <abotsistanley0@gmail.com>
2022-10-29 05:47:00 +0000

    crud

--- src/main.rs
+++ src/main.rs
@@ -132,27 +132,6 @@ fn main() -> Result<(), Error> {
         }
         //    break;
     }
-    //     client.batch_execute("
-    //     CREATE TABLE personname (
-    //         id      SERIAL PRIMARY KEY,
-    //         name    TEXT NOT NULL,
-    //         data    BYTEA
-    //     )
-    // ")?;
-
-    // let name = "Ferris";
-    // let data = None::<&[u8]>;
-    // client.execute(
-    //     "INSERT INTO person (name, data) VALUES ($1, $2)",
-    //     &[&name, &data],
-    // )?;
-
-    // for row in client.query("SELECT id, name, data FROM person", &[])? {
-    //     let id: i32 = row.get(0);
-    //     let name: &str = row.get(1);
-    //     let data: Option<&[u8]> = row.get(2);
-
-    //     println!("found person: {} {} {:?}", id, name, data);
-    // }
+    
     Ok(())
 }
